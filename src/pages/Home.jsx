2bf5031978cc6dd3c610227b749c8f1874f127bd
Morad17 <morad123@hotmail.co.uk>
2024-11-27 12:37:26 +0000

    finished expenses form

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,10 +4,10 @@ import { Container, Row, Col, Form } from 'react-bootstrap'
 const Home = () => {
   return (
     <Container className="home" >
-        <Row className="intro" >
-            <h1>Project Expense Tracker</h1>
+        <Row className="intro justify-content-center text-center m-4" >
+            <h1 className="mb-4">Project Expense Tracker</h1>
             <h3>Please Enter Your Total Budget For the Project</h3>
-            <Col xs className="">
+            <Col xs="8" className="">
                 <Form>
                     <Form.Group>
                         <Form.Control type="number" placeholder="Total Budget"/>
@@ -19,36 +19,57 @@ const Home = () => {
             <h1>Expenses</h1>
             <h3>Please Fill in all expected expenses for the Project</h3>
             <Form>
-                <Form.Group as={Row}>
-                    <Form.Label>
+                <Form.Group  className="mt-2 justify-content-center"as={Row}>
+                    <Form.Label column sm="1">
                         Venue
                     </Form.Label>
+                    <Col sm="5">
                     <Form.Control type="number"/>
-                    <Form.Label>
-                        Equipment
+                    </Col>
+                </Form.Group>
+                <Form.Group className="mt-2 justify-content-center"as={Row}>
+                    <Form.Label column sm="1">
+                        Equipmen
                     </Form.Label>
+                    <Col sm="5">
                     <Form.Control type="number"/>
-                    <Form.Label>
+                    </Col>
+                </Form.Group><Form.Group className="mt-2 justify-content-center"as={Row}>
+                    <Form.Label column sm="1">
                         Performers
                     </Form.Label>
+                    <Col sm="5">
                     <Form.Control type="number"/>
-                    <Form.Label>
+                    </Col>
+                </Form.Group><Form.Group className="mt-2 justify-content-center"as={Row}>
+                    <Form.Label column sm="1">
                         Staff
                     </Form.Label>
+                    <Col sm="5">
                     <Form.Control type="number"/>
-                    <Form.Label>
+                    </Col>
+                </Form.Group><Form.Group className="mt-2 justify-content-center"as={Row}>
+                    <Form.Label column sm="1">
                         Managerial
                     </Form.Label>
+                    <Col sm="5">
                     <Form.Control type="number"/>
-                    <Form.Label>
+                    </Col>
+                </Form.Group><Form.Group className="mt-2 justify-content-center"as={Row}>
+                    <Form.Label column sm="1">
                         Marketing
                     </Form.Label>
+                    <Col sm="5">
                     <Form.Control type="number"/>
-                    <Form.Label>
+                    </Col>
+                </Form.Group><Form.Group className="mt-2 justify-content-center"as={Row}>
+                    <Form.Label column sm="1">
                         Utility
                     </Form.Label>
+                    <Col sm="5">
                     <Form.Control type="number"/>
-                </Form.Group>
+                    </Col>
+                </Form.Group>   
             </Form>
             <Col>
 
